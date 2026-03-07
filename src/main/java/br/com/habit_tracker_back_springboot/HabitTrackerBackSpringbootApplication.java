d79cc6d6404353ac02bd574df329e8ffdbbe9842
leandroltfx <leandroltfbr@hotmail.com>
2026-03-07 08:03:39 -0300

    Inicia o projeto

---
+++ src/main/java/br/com/habit_tracker_back_springboot/HabitTrackerBackSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.habit_tracker_back_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitTrackerBackSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitTrackerBackSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/habit_tracker_back_springboot/HabitTrackerBackSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.habit_tracker_back_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerBackSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
